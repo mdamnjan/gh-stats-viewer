ac8862360687c02935e016e1f451ae6590fb7c01
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-06-07 03:42:37 -0400

    Add number charts to overview

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { BarChartLine, JournalCode } from "react-bootstrap-icons";
 
 import "./App.css";
-import Card from "./components/Card/Card";
 import Tabs from "./components/Tabs/Tabs";
 import ProfileSideBar from "./components/Profile/ProfileSideBar";
 
@@ -17,9 +16,13 @@ function App() {
   const [user, setUser] = useState(null);
 
   const getRepos = async () => {
-    const repos = await octokit.request(`GET /users/${process.env.REACT_APP_GH_USER}/repos`);
+    const repos = await octokit.request(
+      `GET /users/${process.env.REACT_APP_GH_USER}/repos`
+    );
     setRepos(repos.data);
-    const user = await octokit.request(`GET /users/${process.env.REACT_APP_GH_USER}`);
+    const user = await octokit.request(
+      `GET /users/${process.env.REACT_APP_GH_USER}`
+    );
     setUser(user.data);
     return repos;
   };
@@ -33,12 +36,31 @@ function App() {
         <div id="container">
           <ProfileSideBar user={user} />
           <div className="contents">
-            <Tabs tabs={[{text: "Overview", active: false, icon: <BarChartLine/>}, {text: "Repositories", active: true, icon: <JournalCode/>}]}/>
-            <div className="repo-list">
-              {repos.map((repo) => (
+            <Tabs
+              tabs={[
+                { text: "Overview", active: false, icon: <BarChartLine /> },
+                { text: "Repositories", active: true, icon: <JournalCode /> },
+              ]}
+            />
+            {/* <div className="repo-list"> */}
+            {/* {repos.map((repo) => (
                 <Card repo={repo} />
-              ))}
+              ))} */}
+            <div className="dashboard">
+              <div data-bs-theme="dark" className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Public Repos</h5>
+                  <h1>{user?.public_repos}</h1>
+                </div>
+              </div>
+              <div data-bs-theme="dark" className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Followers</h5>
+                  <h1>{user?.followers}</h1>
+                </div>
+              </div>
             </div>
+            {/* </div> */}
           </div>
         </div>
       </header>
