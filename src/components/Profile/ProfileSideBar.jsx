cfe34923921bbea4c0c1b32063ee4e440b046d9c
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-06-07 02:22:31 -0400

    Display date user joined

--- src/components/Profile/ProfileSideBar.jsx
+++ src/components/Profile/ProfileSideBar.jsx
@@ -4,8 +4,18 @@ const ProfileSideBar = ({ user }) => {
   return (
     <div data-bs-theme="dark" className="user-profile">
       <img id="user-avatar" alt="User's avatar" src={user?.avatar_url}></img>
-      <h4>{user?.login}</h4>
-      <h6>{user?.bio}</h6>
+      <h4 className="h4">{user?.login}</h4>
+      <h6 className="h6">{user?.bio}</h6>
+      <h6 className="h6">
+        <small class="text-muted">
+          Joined{" "}
+          {new Date(user?.created_at).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "2-digit",
+          })}
+        </small>
+      </h6>
     </div>
   );
 };
