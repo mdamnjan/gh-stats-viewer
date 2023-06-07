e0a8c1108e1cfc17f3a2750fe4cb2e834c434334
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-06-07 02:06:58 -0400

    Add styling to github bio

--- src/components/Profile/ProfileSideBar.jsx
+++ src/components/Profile/ProfileSideBar.jsx
@@ -5,7 +5,7 @@ const ProfileSideBar = ({ user }) => {
     <div data-bs-theme="dark" className="user-profile">
       <img id="user-avatar" alt="User's avatar" src={user?.avatar_url}></img>
       <h4>{user?.login}</h4>
-      <h4>{user?.bio}</h4>
+      <h6>{user?.bio}</h6>
     </div>
   );
 };
