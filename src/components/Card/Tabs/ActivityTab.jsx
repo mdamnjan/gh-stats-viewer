8ea059af629f54be82a0de237fe3599b01cd6b9a
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-06-07 20:29:20 -0400

    Add line chart with commit count to activities tab

--- src/components/Card/Tabs/ActivityTab.jsx
+++ src/components/Card/Tabs/ActivityTab.jsx
@@ -1,9 +1,35 @@
 import BaseTab from "./BaseTab";
+import "./Tabs.css";
+
+import { Octokit } from "octokit";
+import { useState, useEffect } from "react";
+import LineChart from "../../Charts/LineBar";
 
 const ActivityTab = ({ repo }) => {
+  const [commits, setCommits] = useState([]);
+
+  useEffect(() => {
+    const fetchCommits = async () => {
+      const commits = await octokit.request(
+        `GET /repos/${process.env.REACT_APP_GH_USER}/${repo.name}/commits`
+      );
+      setCommits(commits.data);
+    };
+
+    fetchCommits();
+  }, []);
+
+  const octokit = new Octokit({
+    // use access token from env for now
+    auth: process.env.REACT_APP_GH_ACCESS_TOKEN,
+  });
   return (
     <BaseTab repo={repo}>
-      <p>Activities</p>
+      <div className="activity-tab-container">
+        <div className="chart-container">
+          <LineChart inputData={commits} />
+        </div>
+      </div>
     </BaseTab>
   );
 };
