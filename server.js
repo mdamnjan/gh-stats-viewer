dd34492c2b7258d3a1d4544b2b4fd69684838ac1
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-06-20 00:25:13 -0400

    Add more endpoints

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import fs from "fs";
 import { App as OctokitApp } from "octokit";
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
+app.use(bodyParser());
 
 // Load environment variables from .env file
 dotenv.config();
@@ -31,15 +33,50 @@ const octokit = await octokitApp.getInstallationOctokit(
   process.env.INSTALLATION_ID
 );
 
-// test to see if the authentication worked
-// const repos = await octokit.request(
-//   `GET /users/${process.env.REACT_APP_GH_USER}/repos`
-// );
+app.get("/profile-stats", async function (req, res) {
+  try {
+    const profileStats = await octokit.request(`GET /users/${req.query.user}`);
+    return res.json(profileStats.data);
+  } catch (error) {
+    console.log(error)
+  }
 
-// console.log(repos.data);
-  
+});
+
+app.get("/events", async function (req, res) {
+  try {
+    const events = await octokit.request(`GET /users/${req.query.user}/events`);
+    return res.json(events.data);
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+app.get("/repo-stats", async function (req, res) {
+  const repoStats = await octokit.request(
+    `GET /repos/${req.query.user}/${req.query.repo}`
+  );
+
+  const languages = await octokit.request(
+    `GET /repos/${req.query.user}/${req.query.repo}/languages`
+  );
+
+  return res.json({...repoStats.data, "languages": languages.data});
+});
+
+app.get("/repos", async function (req, res) {
+  const repos = await octokit.request(`GET /users/${req.query.user}/repos`);
+  return res.json(repos.data);
+});
+
+app.get("/commits", async function (req, res) {
+  const commits = await octokit.request(
+    `GET /repos/${req.query.user}/${req.query.repo}/commits`
+  );
+
+  return res.json(commits.data);
+});
 
 app.listen(4000, function () {
   console.log("CORS is running on port 4000");
 });
-
