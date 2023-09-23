5b99393073e4bf8a44db61af4d4f8745ebeba60d
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-09-23 00:42:51 -0400

    Add loading state for profile sidebar

--- client/src/components/Profile/ProfileSideBar.jsx
+++ client/src/components/Profile/ProfileSideBar.jsx
@@ -1,21 +1,42 @@
 import "./ProfileSideBar.css";
 
 const ProfileSideBar = ({ user }) => {
+  if (user) {
+    return (
+      <div data-bs-theme="dark" className="user-profile">
+        <img id="user-avatar" alt="User's avatar" src={user?.avatar_url}></img>
+        <div className="user-details">
+          <h4 className="h4"><span>{user?.login}</span></h4>
+          <h6 className="h6"><span>{user?.bio}</span></h6>
+          <h6 className="h6">
+            <small class="text-muted">
+              Joined{" "}
+              {new Date(user?.created_at).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              })}
+            </small>
+          </h6>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div data-bs-theme="dark" className="user-profile">
+    <div data-bs-theme="dark" className="user-profile placeholder-glow">
       <img id="user-avatar" alt="User's avatar" src={user?.avatar_url}></img>
-      <h4 className="h4">{user?.login}</h4>
-      <h6 className="h6">{user?.bio}</h6>
-      <h6 className="h6">
-        <small class="text-muted">
-          Joined{" "}
-          {new Date(user?.created_at).toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "short",
-            day: "2-digit",
-          })}
-        </small>
-      </h6>
+      <div className="user-details">
+        <h4 className="h4 placeholder col-4">
+          <span />
+        </h4>
+        <h6 className="h6 placeholder col-8">
+          <span />
+        </h6>
+        <h6 className="h6 placeholder col-2">
+          <span />
+        </h6>
+      </div>
     </div>
   );
 };
