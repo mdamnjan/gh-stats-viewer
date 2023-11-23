f62e6d405de9f49457ca665cdc20e8c444f42b77
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-11-23 03:49:28 -0500

    Address review comments

--- client/src/components/Search/UserList.jsx
+++ client/src/components/Search/UserList.jsx
@@ -1,11 +1,43 @@
-import SearchItem from "./SearchItem";
+import Card from "../Card/Card";
+
+import { useNavigate } from "react-router-dom";
 
 const UserList = ({ users, isLoading }) => {
+  const navigate = useNavigate();
+
   if (isLoading) {
-    <SearchItem type="users" />
+    return <Card key={1} isLoading={isLoading} />;
   }
   return users.map((user) => {
-    return <SearchItem type="users" data={user} />;
+    return (
+      <div
+        tabIndex={0}
+        onClick={() => navigate(`/users/${user.login}`)}
+        onKeyDown={(e) => {
+          console.log(e.code);
+          if (e.code === "Enter") {
+            navigate(`/users/${user.login}`);
+          }
+        }}
+        data-bs-theme="dark"
+        key={`user-${user.login}`}
+        className="card search-result"
+      >
+        <img
+          className="user-img"
+          alt="user's avatar"
+          src={user.avatar_url}
+        ></img>
+        <div>
+          <h1 style={{ display: "block" }}> {user.login}</h1>
+          {user.type === "Organization" && (
+            <span className="badge rounded-pill text-bg-secondary">
+              {user.type}
+            </span>
+          )}
+        </div>
+      </div>
+    );
   });
 };
 export default UserList;
